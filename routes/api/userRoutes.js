214eb492102fd6381d8d0fbbedcecd7585e9b502
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-03-02 19:38:43 -0600

    Find user by ID is working

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,21 +9,9 @@ const {
   removeFriend
 } = require('../../controllers/userController');
 
-// // /api/courses
-// router.route('/').get(getCourses).post(createCourse);
 
-// /api/courses/:courseId
-// router
-  // .route('/:courseId')
-  // router.get('/users', getUsers);
-  // router.get('/users/:userId', getSingleUser);
-  // router.post('/users', createUser);
-  // router.delete('/users/:userId', deleteUser);
-  // router.put('/users/:userId', updateUser);
-  // router.post('/users/:userId/friends', addFriend);
-  // router.delete('/users/:userId/friends/:friendId', removeFriend);
   router.route("/").get(getUsers).post(createUser);
-  router.route("/:id").get(getSingleUser).put(updateUser).delete(deleteUser);
-  router.route('/:userId').get(getSingleUser);
+  // router.route("/:id").get(getSingleUser).put(updateUser).delete(deleteUser);
+  router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
   router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 module.exports = router;
